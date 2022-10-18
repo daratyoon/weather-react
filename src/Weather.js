891102e44212895d35966ff16298c60a2b7ce36e
daratyoon <sandratsaiiyun@yahoo.com>
2022-10-18 11:48:32 -0500

    Styling files

--- src/Weather.js
+++ src/Weather.js
@@ -15,13 +15,22 @@ export default function Weather(props) {
 
   return (
     <div className="Weather">
-      <h1>Houston</h1>
-      <ul>
-        <li>58°F</li>
-        <li>Mostly Sunny</li>
-        <li>Tuesday, October 18, 2022</li>
-        <li>11:25AM</li>
-      </ul>
+      <div className="col-6">
+        <h1>Houston</h1>
+        <ul>
+          <li>58°F</li>
+          <li>Mostly Sunny</li>
+          <li>Tuesday, October 18, 2022</li>
+          <li>11:25AM</li>
+        </ul>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Precipitation: 10% </li>
+          <li>Wind Speed: 15mph</li>
+          <li>Humidity: 53%</li>
+        </ul>
+      </div>
     </div>
   );
 }
