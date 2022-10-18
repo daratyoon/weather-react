c4a0398b4f2efc16c68fce5b86241461333c2e63
daratyoon <sandratsaiiyun@yahoo.com>
2022-10-17 22:32:40 -0500

    Added div for Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import SearchEngine from "./SearchEngine";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <SearchEngine />
-        <Weather city="Houston" />
-        <a
-          className="App-link"
-          href="https://github.com/daratyoon/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-Sourced Code
-        </a>
-        <span>by Sandra Tsai-Yun</span>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <SearchEngine />
+          <Weather city="Houston" />
+          <a
+            className="App-link"
+            href="https://github.com/daratyoon/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Sourced Code
+          </a>
+          <span>by Sandra Tsai-Yun</span>
+        </header>
+      </div>
     </div>
   );
 }
