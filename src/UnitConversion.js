baa703d8fc4bab6d0404281292e757dcaeada5a0
daratyoon <sandratsaiiyun@yahoo.com>
2022-10-24 22:50:49 -0500

    Created new component for unit conversion

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function UnitConversion(props) {
-  return (
-    <div className="UnitConversion">
-      <span className="DailyTemp">{props.fahrenheit}°</span>{" "}
-      <span className="Unit">°F|°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("fahrenheit");
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  if (unit === "fahrenheit") {
+    return (
+      <div className="UnitConversion">
+        <span className="DailyTemp">{props.fahrenheit}</span>{" "}
+        <span className="Unit">
+          °F|
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let celsius = ((props.fahrenheit - 32) * 5) / 9;
+    return (
+      <div className="UnitConversion">
+        <span className="DailyTemp">{celsius}</span>{" "}
+        <span className="Unit">
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+          | °C
+        </span>
+      </div>
+    );
+  }
 }
