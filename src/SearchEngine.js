2da67644a0eb4a38f197f10712de683eb8d33dc8
daratyoon <sandratsaiiyun@yahoo.com>
2022-10-28 15:36:02 -0500

    Moved Forecast component into SearchEngine component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./SearchEngine.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function SearchEngine(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -63,6 +64,7 @@ export default function SearchEngine(props) {
           </div>
         </form>
         <WeatherInfo data={weatherInfo} />
+        <WeatherForecast />
       </div>
     );
   } else {
