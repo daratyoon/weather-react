4fedeb30328611b614cfc04d3d51600f5154addd
daratyoon <sandratsaiiyun@yahoo.com>
2022-11-02 19:19:08 -0500

    Refactored code and styled button

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -53,11 +53,7 @@ export default function SearchEngine(props) {
               />
             </div>
             <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-outline-info"
-              />
+              <input type="submit" value="Search" className="btn btn-light" />
             </div>
           </div>
         </form>
